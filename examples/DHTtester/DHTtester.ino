dbd27bf54f73100ca9056df95ab0fcb2db5c46a8
qitas <qitas@qitas.cn>
2021-05-18 16:05:45 +0800

    try

--- examples/DHTtester/DHTtester.ino
+++ examples/DHTtester/DHTtester.ino
@@ -1,10 +1,3 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
-
-// REQUIRES the following Arduino libraries:
-// - DHT Sensor Library: https://github.com/adafruit/DHT-sensor-library
-// - Adafruit Unified Sensor Lib: https://github.com/adafruit/Adafruit_Sensor
-
 #include "DHT.h"
 
 #define DHTPIN 2     // Digital pin connected to the DHT sensor
